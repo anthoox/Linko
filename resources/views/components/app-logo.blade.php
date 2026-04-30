000a8268d62e4653d87bd4259b0c70aacefc39c2
Kray <kray.a4@gmail.com>
2026-04-30 11:56:05 +0200

    ux: modificando nombre web

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -9,9 +9,12 @@
     </x-slot>
 </flux:sidebar.brand>
 @else
-<flux:brand name="Linko" {{ $attributes }}>
-    <x-slot name="logo" class="flex aspect-square dark:bg-dub-surface bg-gray-100 size-13 items-center justify-center rounded-md  text-accent-foreground">
-        <x-app-logo-icon class="size-5 fill-current  text-white dark:text-black" />
+<flux:brand {{ $attributes }}>
+    <x-slot name="logo" class="flex aspect-square dark:bg-dub-surface bg-gray-100 size-13 items-center justify-center rounded-md">
+        <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
     </x-slot>
 </flux:brand>
+    <span class="text-xl lg:text-2xl font-semibold me-4">
+        Linko
+    </span>
 @endif
